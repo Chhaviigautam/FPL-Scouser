5087fb209aac628025e90a0c9b8fe6c6c61e84a3
Your Name <your_email@example.com>
2026-02-26 12:02:17 +0530

    change by favian

--- fpl-app/frontend/src/pages/ModelInsights.jsx
+++ fpl-app/frontend/src/pages/ModelInsights.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import { api } from "../api/client";
+import { api } from "../api/client";   //connecting backend with frontend
 
 const FEATURE_LABELS = {
   avg_pts_last3:          "Avg Pts (3GW)",
